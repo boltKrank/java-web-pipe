8144a6175443050738964358f8ff88d81ac42735
Simon Anderson <simon.anderson111@gmail.com>
2018-05-30 11:37:47 +0900

    updated tests

---
+++ src/test/java/org/gradle/demo/HelloServletFunctionalTest.java
@@ -0,0 +1,45 @@
+package org.gradle.demo;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloServletFunctionalTest {
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        ChromeDriverManager.getInstance().setup();
+    }
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null)
+            driver.quit();
+    }
+
+    @Test
+    public void sayHello() throws Exception {
+        driver.get("http://localhost:8080/java-web-pipe");
+
+        driver.findElement(By.id("say-hello-text-input")).sendKeys("Dolly");
+        driver.findElement(By.id("say-hello-button")).click();
+
+        assertEquals("Hello Page", driver.getTitle());
+        assertEquals("Hello, Dolly!", driver.findElement(By.tagName("h2")).getText());
+    }
+}
